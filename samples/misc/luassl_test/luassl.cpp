1d37d2ac664af954d58993d7afe65d809755ee83
_null_ <ul.null.ul@gmail.com>
2013-05-12 17:49:37 -0400

    luassl fixed

---
+++ samples/misc/luassl_test/luassl.cpp
@@ -0,0 +1,45 @@
+#define GVN_ACTIVATE_LOGGER
+
+#include <graviton.hpp>
+#include <iostream>
+#include <core/luaviton.hpp>
+#include <core/luaviton_ssl.hpp>
+
+using namespace std;
+
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+/// Run a Lua Test
+int main ( int argc , char **argv)
+{
+    string file;// = "test.lua";
+
+    GraVitoN::Core::Luaviton        my_lua;
+    // GraVitoN::Core::Luaviton_Socket    luasoc(my_lua);
+    GraVitoN::Core::Luaviton_SSL    luassl(my_lua);
+
+    // luasoc.registerModule();
+    luassl.registerModule();
+
+    // luasoc.loadEmAll();
+    luassl.loadEmAll();
+
+    cout << " done" << endl;
+
+    if(argc != 2)
+    {
+        cout<<"Usage: "<<argv[0]<<" [lua script file]"<<endl;
+        return 0;
+    }
+
+    file = argv[1];
+
+    /// Run script file: test.lua
+    cout << " Running script ...";
+    my_lua.runScriptFile(file);
+    cout << " done" << endl;
+
+    // my_lua.free(my_lua);
+
+    return 0;
+}
