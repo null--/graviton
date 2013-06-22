3335d10b63f8cd545c929d7225361ce1949c5286
_null_ <ul.null.ul@gmail.com>
2013-06-22 08:43:06 +0430

    netkit --> sokcet

--- code/core/socket.hpp
+++ code/core/socket.hpp
@@ -21,7 +21,7 @@
  * along with Graviton.  If not, see http://www.gnu.org/licenses/.
  *
  * @brief GraVitoN::Core::Socket
-*/
+ */
 
 #ifndef GRAVITON_SOCKET_H
 #define GRAVITON_SOCKET_H
@@ -30,16 +30,16 @@
 #include <core/component.hpp>
 
 #if defined(INFO_OS_WINDOWS)
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #else
-    #include <stdint.h>
-    #include <netdb.h>
-    #include <netinet/in.h>
-    #include <sys/socket.h>
-    #include <arpa/inet.h>
-    #include <sys/types.h>
-    #include <unistd.h>
+#include <stdint.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
 #endif
 
 namespace GraVitoN
@@ -50,6 +50,30 @@ namespace GraVitoN
         class Socket : public Component_Singleton<Socket> 
         {
         public:
+            /// is str a valid ipv4 string
+            static bool isAValidIPv4(const string &str);
+
+            /// DNS domain lookup (using IPv4)
+            static string dnsLookup4(const std::string &domain);
+
+            /// host to network: 32bit
+            static unsigned int hostToNet_32(const unsigned int &val);
+
+            /// host to network: 16bit
+            static unsigned int hostToNet_16(const unsigned short &val);
+
+            /// network to host: 32 bit
+            static unsigned int netToHost_32(const unsigned int &val);
+
+            /// network to host: 16 bit
+            static unsigned int netToHost_16(const unsigned short &val);
+
+            /// Convert a hexadecimal IPv4 representation to a string representation
+            static string hexToStrIPv4(const unsigned int hex);
+
+            /// Convert a string IPv4 representation to a hexadecimal representation
+            static unsigned int strToHexIPv4(string str);
+                
 #ifdef INFO_OS_WINDOWS
             typedef SOCKET Handle;
             typedef int Size;
@@ -98,6 +122,102 @@ namespace GraVitoN
         Socket::~Socket()
         {
             /// NO NEED FOR WSACleanUp
+        }
+
+        //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+        bool Socket::isAValidIPv4(const string &str)
+        {
+            if( str.size() < 7 )
+                return false;
+            if( str.size() > 15 )
+                return false;
+
+            int dot = 0;
+            for(size_t i = 0; i < str.size(); ++i)
+                if( str[i] == '.' )
+                    ++dot;
+                else if( !isdigit(str[i]) )
+                    return false;
+            return dot == 3;
+        }
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+        string Socket::dnsLookup4(const std::string &domain)
+        {
+            //for(int i=0; domain[i] != '\0'; ++i)
+            //    cout << (int)domain[i] << endl;
+
+            hostent *m_hostent;
+            m_hostent = gethostbyname( domain.c_str() );
+            if( !m_hostent )
+                return "";
+            unsigned int addr;
+            memcpy((void*)&addr, (void*)*m_hostent->h_addr_list, 4);
+
+            return string(
+                inet_ntoa(
+                    *(in_addr*)&addr
+                    )
+                );
+        }
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+        unsigned int Socket::hostToNet_32(const unsigned int &val)
+        {
+            return ntohl(val);
+        }
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+        unsigned int Socket::hostToNet_16(const unsigned short &val)
+        {
+            return htons(val);
+        }
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+        unsigned int Socket::netToHost_32(const unsigned int &val)
+        {
+            return ntohl(val);
+        }
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+        unsigned int Socket::netToHost_16(const unsigned short &val)
+        {
+            return ntohs(val);
+        }
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+        string Socket::hexToStrIPv4(const unsigned int hex)
+        {
+            if(!hex)
+                return "";
+
+            char ipc[16];
+            unsigned char hip[4];
+            memcpy((void*)hip, (void*)&hex, 4);
+            snprintf(ipc, sizeof(ipc), "%u.%u.%u.%u",
+                     (unsigned int)hip[3],
+                     (unsigned int)hip[2],
+                     (unsigned int)hip[1],
+                     (unsigned int)hip[0]);
+            return string(ipc);
+
+        }
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+        unsigned int Socket::strToHexIPv4(string str)
+        {
+            string res;
+
+            unsigned int hexip;
+            unsigned char hex[4];
+            sscanf (str.c_str(), "%u.%u.%u.%u",
+                    (unsigned int *)&hex[3],
+                    (unsigned int *)&hex[2],
+                    (unsigned int *)&hex[1],
+                    (unsigned int *)&hex[0]);
+
+            memcpy((void*)&hexip, (void*)hex, 4);
+            return hexip;
         }
     } /// end of Core
 } /// end of GraVitoN
