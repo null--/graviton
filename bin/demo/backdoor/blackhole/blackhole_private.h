d612f1775b1e24ec5ddf86a586d3c9287268db40
null-- <sinahatef.cpp@gmail.com>
2015-10-05 02:24:01 +0330

    RAT Mode - Alpha

--- bin/demo/backdoor/blackhole/blackhole_private.h
+++ bin/demo/backdoor/blackhole/blackhole_private.h
@@ -10,14 +10,14 @@
 #define VER_MINOR	0
 #define VER_RELEASE	0
 #define VER_BUILD	0
-#define COMPANY_NAME	""
-#define FILE_VERSION	"1.0.0.0"
-#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define COMPANY_NAME	"Microsoft"
+#define FILE_VERSION	"8.0.0.0"
+#define FILE_DESCRIPTION	"Windows Explorer"
 #define INTERNAL_NAME	""
-#define LEGAL_COPYRIGHT	""
-#define LEGAL_TRADEMARKS	""
+#define LEGAL_COPYRIGHT	"Microsoft"
+#define LEGAL_TRADEMARKS	"Microsoft"
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"1.0.0.0"
+#define PRODUCT_VERSION	"8.0.0.0"
 
 #endif /*BLACKHOLE_PRIVATE_H*/
